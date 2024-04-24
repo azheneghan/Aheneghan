546eed2d766de5566c4fb171c5d91e72fd3ad121
azheneghan <112512108+azheneghan@users.noreply.github.com>
2024-04-24 00:18:19 -0400

    Add files via upload

--- proj/app.R
+++ proj/app.R
@@ -9,6 +9,13 @@
 
 library(shiny)
 
+##  Working data set
+penguin0 <- na.omit(read.csv(file="https://raw.githubusercontent.com/azheneghan/aheneghan/main/datasets/w03-penguins.csv"))
+penguin0$BMI = (penguin0$body_mass_g/4000)
+x.names = names(penguin0)[-c(1,2,3,7,8,9)]
+y.names =names(penguin0)[-c(1,2,3,7,8,9)]
+##
+#UI
 ui <- fluidPage(
   withMathJax(),
   tags$div(HTML("<script type='text/x-mathjax-config' >
@@ -19,55 +26,135 @@ ui <- fluidPage(
             ")),
   ### 
   br(), br(), 
-  wellPanel(style = "background-color:lightskyblue;",  
-            # App title ----
-            titlePanel(h2("Simulating Binomial Distribution", align = "center",
+  wellPanel(style = "background-color:lightskyblue;",
+            titlePanel(h2("Analysis of Penguin Data", align = "center",
                           style = "color:midnightblue; font-family:verdana; 
-                      font-variant: small-caps; font-weight: bold;"))),
-  ###
-  sidebarLayout(    # siderbarLayout:       
-    sidebarPanel( style = "background-color:lightskyblue;", 
-                  tags$div( h4("User Input Panel", align = "center",
-                               style = "color:darkred; font-family:verdana; 
                       font-variant: small-caps; font-weight: bold;")),
-                  #
-                  br(),
-                  ##
-                  # slider input: number of experiments
-                  sliderInput(inputId = "x",
-                              label = "Number of Experiments",
-                              min = 1,
-                              max = 1000,
-                              value = 500),
-                  # slider input: number of trials per experiment
-                  sliderInput(inputId = "n",
-                              label = "Number of Trials",
-                              min = 1,
-                              max = 100,
-                              value = 50),
-                  # slider input: probability of success
-                  sliderInput(inputId = "p",
-                              label = "Probability of Success",
-                              min = 0,
-                              max = 1,
-                              value = 0.5)),
-    mainPanel( style = "background-color:khaki1; text-align: center;", 
-               plotOutput(outputId = "distPlot")
+  ),
+  ###
+  sidebarLayout(
+    sidebarPanel(style = "background-color:lightskyblue;", 
+                 radioButtons(inputId = "island", 
+                              label = "Location",
+                              choices = c("Biscoe",
+                                          "Dream",
+                                          "Torgersen",
+                                          "All"),
+                              inline = FALSE,
+                              selected = "All"),
+                 br(),
+                 ###
+                 selectInput(inputId = "Y",
+                             label = "Response Variable: Y",
+                             choices = y.names),
+                 
+                 selectInput(inputId = "X",
+                             label =  "Predictor Variable: X",
+                             choices = x.names,
+                             selected = x.names[2]),
+                 ####
+                 hr(),
+                 ###
+                 sliderInput(inputId = "newX", 
+                             label = "New Value for Prediction:", 
+                             value = 117, 
+                             min = 0, 
+                             max = 235, 
+                             step = 0.1),
+    ),
+    ###
+    mainPanel(style = "background-color:lightgray; text-align: center;", 
+              tabsetPanel(type = "tabs",
+                          tabPanel(h5("Scatter Plot", style ="color:darkred;"), plotOutput("plot")),
+                          tabPanel(h5("Regression Coefficients", style ="color:darkred;"), tableOutput("table")),
+                          tabPanel(h5("Diagnostics", style ="color:darkred;"), plotOutput("diagnosis")),
+                          tabPanel(h5("Prediction", style ="color:darkred;"), plotOutput("predPlt"))
+              )
+              
     )
-  ))
-###
+  )
+)
+
+## Server function
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
+  #### Subsetting data based on Island Location
+  workDat = function(){
+    if (input$island == "Biscoe") {
+      workingData = penguin0[which(penguin0$island == "Biscoe"),]
+    } else if (input$island == "Dream") {
+      workingData = penguin0[which(penguin0$island == "Dream"),]
+    } else if (input$island == "Torgersen") {
+      workingData = penguin0[which(penguin0$island == "Torgersen"),]
+    } else {
+      workingData = penguin0
+    }
+    workingData 
+  }
+  ######################################
+  #######   Scatter Plot
+  ######################################
+  output$plot <- renderPlot({
+    par(bg="khaki1")
+    dataset = workDat()[,-1]
+    #####
+    plot(dataset[,input$X], dataset[,input$Y], 
+         xlab = input$X,
+         ylab = input$Y,
+         main = paste("Relationship between", input$Y, "and", input$X)
+    )
     ##
-    binom.score <- rbinom(input$x, input$n, input$p)
+    abline(lm(dataset[,input$Y] ~ dataset[,input$X]), col = "blue", lwd = 2)
+  })
+  
+  ######################################
+  #######   Regression Coefficiients
+  ######################################
+  output$table <- renderTable({
+    br()
+    br()
+    dataset = workDat()[,-1]
+    m0 = lm(dataset[,input$Y] ~ dataset[,input$X])
+    #summary(m0)
+    regcoef = data.frame(coef(summary(m0)))
+    
     ##
-    par(bg = "khaki1")
-    hist(binom.score, probability = TRUE, col = "deeppink", border = "darkgreen",
-         xlab = "Histogram of Generated Binomial Data",
-         main = "Simulated Binomial Distribution")
-    d <- density(binom.score) # returns the density data
-    lines(d)
-  }, height = 400, width = 800)
+    regcoef$Pvalue = regcoef[,names(regcoef)[4]]
+    ###
+    regcoef$Variable = c("Intercept", input$X)
+    regcoef[,c(6, 1:3, 5)]
+    
+  })
+  ######################################
+  #######   Diagnostics
+  ######################################
+  output$diagnosis <- renderPlot({
+    par(bg="khaki1")
+    dataset = workDat()[,-1]
+    #####
+    m1=lm(dataset[,input$Y] ~ dataset[,input$X])
+    par(mfrow=c(2,2))
+    plot(m1)
+  })   
+  
+  ######################################
+  #######       Prediction Plot
+  ######################################
+  output$predPlt <- renderPlot({
+    par(bg="khaki1")
+    dataset = workDat()[,-1]
+    ###
+    m3 = lm(dataset[,input$Y] ~ dataset[,input$X])
+    pred.y = coef(m3)[1] + coef(m3)[2]*input$newX
+    #####
+    plot(dataset[,input$X], dataset[,input$Y], 
+         xlab = input$X,
+         ylab = input$Y,
+         main = paste("Relationship Between", input$Y, "and", input$X)
+    )
+    ##
+    abline(m3, col = "deeppink", lwd = 1, lty=2)
+    points(input$newX, pred.y, pch = 19, col = "deeppink", cex = 2)
+  })
 }
 ###
 shinyApp(ui = ui, server = server)
